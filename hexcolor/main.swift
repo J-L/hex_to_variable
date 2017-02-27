771adc0ef83a7c050109ccfa3686b1bafc58c94b
John Lee <johnjustinlee@me.com>
2017-02-27 13:25:14 +0700

    Initial Commit

---
+++ hexcolor/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  hexcolor
+//
+//  Created by John Lee on 2017-02-27.
+//  Copyright © 2017 johnlee. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
